cfbe3ea61f7295895021d3e0810d140c27e02000
Neology92 <oskarlegner@gmail.com>
2019-04-01 18:25:40 +0200

    fixed introSort

--- src/introSort.cpp
+++ src/introSort.cpp
@@ -5,7 +5,7 @@
 void iSortAsc(std::vector<int> &arr, int l_ind, int r_ind, int deep)
 {
     int m_ind;
-        if(r_ind - l_ind > 9 && deep > 0)
+        if(l_ind < r_ind && deep > 0)
         {
             m_ind = partitionAsc(arr, l_ind, r_ind);
             iSortAsc(arr, l_ind, m_ind-1, deep-1);      // Na lewo od pivota
@@ -21,7 +21,7 @@ void iSortAsc(std::vector<int> &arr, int l_ind, int r_ind, int deep)
 void iSortDesc(std::vector<int> &arr, int l_ind, int r_ind, int deep)
 {
     int m_ind;
-        if(r_ind - l_ind > 9 && deep > 0)
+        if(l_ind < r_ind && deep > 0)
         {
             m_ind = partitionDesc(arr, l_ind, r_ind);
             iSortDesc(arr, l_ind, m_ind-1, deep-1);         // Na lewo od pivota
